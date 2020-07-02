da08eda4b83c0cb2786058fd4c2b7f703c1986f3
Jiun Kim <jkim2@bowdoin.edu>
2020-07-02 23:35:31 +0900

    upload to aws lambda

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from bot import telegram_chatbot
-import gizoogle
 
 bot = telegram_chatbot("config.cfg")
 
@@ -10,23 +9,25 @@ def make_reply(msg):
         reply = msg
     return reply
 
-update_id = None
-while True:
-    updates = bot.get_updates(offset=update_id)
-    updates = updates["result"]
-    if updates:
-        for item in updates:
-            update_id = item["update_id"]
-            try:
-                message = str(item["message"]["text"])
-            except:
-                message = None
-            from_ = item["message"]["from"]["id"]
-            if message == 'Where am I?':
-                message = bot.get_location()
-            elif message == 'Github':
-                message = bot.get_github()
-            else:
-                message = '뭐라 해드릴 말이 없군요...'
-            reply = make_reply(message)
-            bot.send_message(reply, from_)
+def lambda_handler(event=None, context=None):
+    update_id = None
+    while True:
+        updates = bot.get_updates(offset=update_id)
+        updates = updates["result"]
+        if updates:
+            for item in updates:
+                update_id = item["update_id"]
+                try:
+                    message = str(item["message"]["text"])
+                except:
+                    message = None
+                from_ = item["message"]["from"]["id"]
+                if message == 'Where am I?':
+                    message = bot.get_location()
+                elif message == 'Github':
+                    message = bot.get_github()
+                else:
+                    message = '뭐라 해드릴 말이 없군요...'
+                reply = make_reply(message)
+                bot.send_message(reply, from_)
+lambda_handler()
